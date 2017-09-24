b84b0d706521266fb6891102aa998cc097ed7c5a
Suyeol Jeon <devxoul@gmail.com>
2017-09-25 03:44:06 +0900

    Fix RxCodableMoya tests

--- Tests/RxCodableMoyaTests/RxCodableMoyaTests.swift
+++ Tests/RxCodableMoyaTests/RxCodableMoyaTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import Moya
 import RxBlocking
-import RxCodable
+import RxCodableMoya
 import RxSwift
 import TestUtil
 
@@ -16,8 +16,8 @@ class RxCodableMoyaTests: XCTestCase {
     let jsonData = jsonString.data(using: .utf8) ?? Data()
     let response = Moya.Response(statusCode: 200, data: jsonData)
     let user = User(id: 123, name: "devxoul")
-////    XCTAssertEqual(try Observable.just(response).map(User.self).toBlocking().first()!, user)
-////    XCTAssertEqual(try Single.just(response).map(User.self).toBlocking().first()!, user)
-////    XCTAssertEqual(try Maybe.just(response).map(User.self).toBlocking().first()!, user)
+    XCTAssertEqual(try Observable.just(response).map(User.self).toBlocking().first()!, user)
+    XCTAssertEqual(try Single.just(response).map(User.self).toBlocking().first()!, user)
+    XCTAssertEqual(try Maybe.just(response).map(User.self).toBlocking().first()!, user)
   }
 }
